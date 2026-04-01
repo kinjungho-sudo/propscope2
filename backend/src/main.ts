75b987146ed2a700269bfa45246e7bb6e2e83aaa
PropScope Master <propscope@youngja.ai>
2026-04-01 18:41:33 +0900

    fix: CORS 전체 허용, Cloudflare Pages 연결 가능하도록 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,7 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({ origin: ['http://localhost:3000'] });
+  app.enableCors({ origin: '*' });
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new ResponseInterceptor());
